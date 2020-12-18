10a7545d3b5c4b9f8976ff066aaa880a6f38d646
Alexander Klutsouk <37481396+Virtouoz@users.noreply.github.com>
2020-12-19 01:18:28 +0300

    lab5

---
+++ Labs/lab5/Debug/main.d
@@ -0,0 +1,31 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: P:/CCS10/ccs/ccs_base/msp430/include/msp430.h
+main.obj: P:/CCS10/ccs/ccs_base/msp430/include/msp430f5529.h
+main.obj: P:/CCS10/ccs/ccs_base/msp430/include/in430.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/math.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_ti_config.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/linkage.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_defs.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_types.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/cdefs.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_types.h
+main.obj: P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_limits.h
+
+../main.c: 
+P:/CCS10/ccs/ccs_base/msp430/include/msp430.h: 
+P:/CCS10/ccs/ccs_base/msp430/include/msp430f5529.h: 
+P:/CCS10/ccs/ccs_base/msp430/include/in430.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics_legacy_undefs.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/math.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_ti_config.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/linkage.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_defs.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_types.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/cdefs.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_types.h: 
+P:/CCS10/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_limits.h: 
